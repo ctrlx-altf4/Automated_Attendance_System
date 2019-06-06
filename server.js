c0cdd458a826990d1e5d46dff2e74031c2fe99de
Prajwal Pradhan <ctrlx.altf4@gmail.com>
2019-06-06 22:04:56 +0545

    ready for heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = require('./config/db');
 // const cv = require('opencv4nodejs');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT ||5000;
 
 app.use(express.static('collection'));
 
